ca43897b7915b8a2189a68e164ee0726f71126b0
yollo <trycollens@gmail.com>
2019-02-22 13:39:47 +0000

    novosib

--- resources/views/regions/sibirkiy/sibirkiy.blade.php
+++ resources/views/regions/sibirkiy/sibirkiy.blade.php
@@ -34,7 +34,7 @@
     <div class="row">
       <div class="col s3"></div>
       <div class="col s6 center">
-        <a class="waves-effect waves-light btn btn__solo">СТАНЬ УЧАСТНИКОМ ПРЯМО СЕЙЧАС</a>
+        <a href="#register" class="waves-effect waves-light btn btn__solo">СТАНЬ УЧАСТНИКОМ ПРЯМО СЕЙЧАС</a>
       </div>
       <div class="col s3"></div>
     </div>
@@ -111,11 +111,12 @@
       <div class="col s12 center">
         <h1 class="registr_solo">РЕГИСТРАЦИЯ</h1>
       </div>
-      <div class="col s2"></div>
-        <div class="col s12 m8">
-          <a class="link-button" href="https://vk.com/doc50857215_484294399?hash=c564e2ebf446865475&dl=9ae04cd1fe1d21480a">Регистрация на мероприятие</a>
+        <div class="col s12 m6">
+          <a class="link-button" href="https://rusinnovations.timepad.ru/event/913586/">Регистрация с проживанием</a>
         </div>
-        <div class="col s2"></div>
+        <div class="col s12 m6">
+          <a class="link-button" href="https://rusinnovations.timepad.ru/event/913585/">Регистрация без проживания</a>
+        </div>
     </div>
   </div>
 </section>
@@ -128,16 +129,17 @@
         <div class="">
           <div class="col s12 m4 line__red ln__1">
           <p class="contact_face">
-            Контактное лицо в Москве:<br>
-Никита Сергеев<br>
-sales@rusinnovations.com<br>
-г. Москва ул. Любойулицы д. 10
+            Контактное лицо в Новосибирске:<br>
+Филатова Светлана Геннадьевна<br>
+s.filatova@corp.nstu.ru <br>
+89139470302 <br>
+Проспект Карла Маркса дом 20
           </p>
           <img src="assets/img/Iogo.png" class="ln_img"></img>
         </div>
         <div class="col s12 m8 line__red ln__2">
           <div id="map">
-          <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A762688ec59991fb5e3f623c146dbf9746d3880a40c923e4c35c1fd8f56ae85e3&amp;source=constructor" width="100%" height="330" frameborder="0"></iframe>
+          <iframe src="https://yandex.ru/map-widget/v1/?ll=82.906335%2C54.987746&mode=poi&poi%5Bpoint%5D=82.906415%2C54.987714&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D110170481580&sll=39.891523%2C59.220496&sspn=0.157242%2C0.044440&text=новосибирск%20Проспект%20Карла%20Маркса%20дом%2020&z=16&amp;source=constructor" width="100%" height="330" frameborder="0"></iframe>
           </div>
         </div>
         </div>
@@ -149,30 +151,17 @@ sales@rusinnovations.com<br>
 <div class="content-section">
 <div class="content-wrapper w-container">
 <div class="header-centered">
-<h2 data-ix="move-up-on-scroll" class="maps_h1 spik_h">РУКОВОДИТЕЛЬ ПРОГРАММЫ</h2>
+<h2 data-ix="move-up-on-scroll" class="maps_h1 spik_h">СПИКЕРЫ</h2>
 </div>
 <div data-ix="move-up-on-scroll" class="w-dyn-list">
     <div class="flex-wrapper w-dyn-items center">
-    <div data-ix="move-to-origin" class="team-member w-dyn-item">
+      <div data-ix="move-to-origin" class="team-member w-dyn-item">
     <div class="team-member-description">
       <h4 class="heading-4">Станислав Сахаров</h4>
       <p class="paragraph">Руководитель программы <br> IT-Start</p>
     </div>
       <img src="assets/img/sp1.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
     </div>
-    </div>
-</div>
-</div>
-</div>
-</section>
-<section id="speakers">
-<div class="content-section">
-<div class="content-wrapper w-container">
-<div class="header-centered">
-<h2 data-ix="move-up-on-scroll" class="maps_h1 spik_h">СПИКЕРЫ</h2>
-</div>
-<div data-ix="move-up-on-scroll" class="w-dyn-list">
-    <div class="flex-wrapper w-dyn-items center">
      <div data-ix="move-to-origin" class="team-member w-dyn-item">
     <div class="team-member-description">
       <h4 class="heading-4">Андрей Поскряков</h4>
@@ -186,34 +175,6 @@ sales@rusinnovations.com<br>
       <p class="paragraph">Руководитель бэк-энд разработки в креативном агентстве SVMD</p>
     </div>
       <img src="assets/img/sp3.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
-    </div>
-    <div data-ix="move-to-origin" class="team-member w-dyn-item">
-    <div class="team-member-description">
-      <h4 class="heading-4">Шариф Кармо</h4>
-      <p class="paragraph">Сооснователь и операционный директор GetShop.TV</p>
-    </div>
-      <img src="assets/img/WhatsApp_Image_2019-02-07_at_17_50_55.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
-    </div>
-    <div data-ix="move-to-origin" class="team-member w-dyn-item">
-    <div class="team-member-description">
-      <h4 class="heading-4">Анна Брусницына</h4>
-      <p class="paragraph">Специалист по социальному предпринимательству</p>
-    </div>
-      <img src="assets/img/c6kLtSyu5Ns.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
-    </div>
-    <div data-ix="move-to-origin" class="team-member w-dyn-item">
-    <div class="team-member-description">
-      <h4 class="heading-4">Кудрин Антон</h4>
-      <p class="paragraph">Старший менеджер Академии PwC</p>
-    </div>
-      <img src="assets/img/Anton-e1472741963931.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
-    </div>
-    <div data-ix="move-to-origin" class="team-member w-dyn-item">
-    <div class="team-member-description">
-      <h4 class="heading-4">Артем Крицын</h4>
-      <p class="paragraph">менеджер проектов АО "РВК"</p>
-    </div>
-      <img src="assets/img/krit.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
     </div>
     </div>
 </div>
@@ -230,15 +191,8 @@ sales@rusinnovations.com<br>
     <div class="flex-wrapper w-dyn-items center">
      <div data-ix="move-to-origin" class="team-member w-dyn-item">
     <div class="team-member-description">
-      <h4 class="heading-4">Владимир Шульгин</h4>
-      <p class="paragraph ">Руководитель проектов <br>АНО "АИР"</p>
-    </div>
-      <img src="assets/img/sp4.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
-    </div>
-    <div data-ix="move-to-origin" class="team-member w-dyn-item">
-    <div class="team-member-description">
-      <h4 class="heading-4 ">Сергеев Никита</h4>
-      <p class="paragraph ">Руководитель проектов <br>АНО "АИР"</p>
+      <h4 class="heading-4">Сергеев Никита</h4>
+      <p class="paragraph ">Контактное лицо в Москве <br><i class="fab fa-whatsapp"></i>8 915 310 34 47  <br> vs@rusinnovations.com</p>
     </div>
       <img src="assets/img/Nikita4.png" alt="" sizes="(max-width: 479px) 100vw, (max-width: 767px) 43vw, 28vw" style="padding-bottom: 130px;">
     </div>
