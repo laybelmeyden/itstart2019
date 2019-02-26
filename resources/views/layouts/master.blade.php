ba8740363d06b7617c4ebee88c1af9434a9efd6e
yollo <trycollens@gmail.com>
2019-02-26 10:24:29 +0000

    metrika

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,8 @@
   <link rel="stylesheet" href="assets/css/anime.css" type="text/css" />
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-
+<meta name="yandex-verification" content="e3c86681ef218fb1" />
+<meta name="google-site-verification" content="sYS9sQCCR4SCLnArtbg7U3BhOcLYEXsOdcjRmIrC2Ug" />
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
