9c3162e0755d84ae53f9b7d91fc96e2e1d9887ef
yollo <trycollens@gmail.com>
2019-04-24 12:28:09 +0000

    ulan

--- resources/views/regions/daliniyvostok/dvos2.blade.php
+++ resources/views/regions/daliniyvostok/dvos2.blade.php
@@ -128,8 +128,8 @@
       </div>
       <div class="col s2"></div>
         <div class="col s12 m8 center">
-          <!--<a class="link-button" href="https://vk.com/doc95748576_492910896?hash=4badcbb4f5faa3a5ce&dl=7bb20338855997eb56" target="_blank">Скачать программу</a>-->
-        <a style="color:white;">Здесь скоро появится Программа стартап-школы</a>
+          <a class="link-button" href="https://vk.com/doc95748576_492910896?hash=4badcbb4f5faa3a5ce&dl=7bb20338855997eb56" target="_blank">Скачать программу</a>
+        <!--<a style="color:white;">Здесь скоро появится Программа стартап-школы</a>-->
         </div>
         <div class="col s2"></div>
     </div>
